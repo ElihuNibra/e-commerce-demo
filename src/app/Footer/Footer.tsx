e25b9687c77198fd87739fb877ea73fd2e009730
Esteve Paredes <esteve.paredes.4@gmail.com>
2024-04-25 11:19:03 -0500

    Fix size anda padding

--- src/app/Footer/Footer.tsx
+++ src/app/Footer/Footer.tsx
@@ -9,9 +9,9 @@ import linkedin from "../../assets/linkedin.svg";
 
 function Footer() {
   return (
-    <footer className="bg-gray-900 text-white mt-[140px] grid grid-cols gap-8">
-      <div className="container max-w-[1320px] py-8 mx-auto grid grid-cols-1 md:grid-cols-5 gap-[87px] items-start">
-        <div className="flex flex-col gap-[16px]">
+    <footer className="bg-gray-900 text-white mt-32 grid grid-cols gap-8 justify-center">
+      <div className="container py-8 grid grid-cols-1 md:px-6 md:grid-cols-2 lg:grid-cols-5 gap-20 items-start">
+        <div className="flex flex-col gap-4">
           <h2 className="text-lg font-bold mb-2">Exclusive</h2>
           <p className="text-gray-100">Subscribe</p>
           <p className="text-gray-100">Get 10% off your first order</p>
@@ -21,7 +21,7 @@ function Footer() {
             placeholder="Enter your email"
           />
         </div>
-        <div className="flex flex-col gap-[16px]">
+        <div className="flex flex-col gap-4">
           <h2 className="text-lg font-bold mb-2">Support</h2>
           <p className="text-gray-100">
             111 Bijoy sarani, Dhaka, DH 1515, Bangladesh.
@@ -29,7 +29,7 @@ function Footer() {
           <p className="text-gray-100">exclusive@gmail.com</p>
           <p className="text-gray-100">+88015-88888-9999</p>
         </div>
-        <div className="flex flex-col gap-[16px]">
+        <div className="flex flex-col gap-4">
           <h2 className="text-lg font-bold mb-2">Account</h2>
           <a className="text-gray-100" href="#">
             My Account
@@ -47,7 +47,7 @@ function Footer() {
             Shop
           </a>
         </div>
-        <div className="flex flex-col gap-[16px]">
+        <div className="flex flex-col gap-4">
           <h2 className="text-lg font-bold mb-2">Quick Link</h2>
           <a className="text-gray-100" href="#">
             Privacy Policy
@@ -62,13 +62,13 @@ function Footer() {
             Contact
           </a>
         </div>
-        <div className="flex flex-col gap-[24px]">
+        <div className="flex flex-col gap-6">
           <h2 className="text-lg font-bold mb-2">Download App</h2>
-          <div className="flex flex-col gap-[8px]">
-            <p className="text-gray-400 text-[12px]">
+          <div className="flex flex-col gap-2">
+            <p className="text-gray-400 text-xs">
               Save $3 with App New User Only
             </p>
-            <div className="grid grid-cols-2 grid-rows-2">
+            <div className="grid grid-cols-2 grid-rows-2 ">
               <Image className="row-span-2" src={qr} alt="qr-icons" />
               <a href="#">
                 <Image src={logoGooglePlay} alt="logoGooglePlay" width={110} />
@@ -78,17 +78,17 @@ function Footer() {
               </a>
             </div>
           </div>
-          <div className="flex gap-[24px]">
-            <a className="w-[24px] h-[24px] flex justify-center" href="#">
+          <div className="flex gap-6">
+            <a className="w-6 h-6 flex justify-center" href="#">
               <Image src={facebook} alt="facebook" height={20} />
             </a>
-            <a className="w-[24px] h-[24px] flex justify-center" href="#">
+            <a className="w-6 h-6 flex justify-center" href="#">
               <Image src={twiter} alt="twiter" height={20} />
             </a>
-            <a className="w-[24px] h-[24px] flex justify-center" href="#">
+            <a className="w-6 h-6 flex justify-center" href="#">
               <Image src={instagram} alt="instagram" height={20} />
             </a>
-            <a className="w-[24px] h-[24px] flex justify-center" href="#">
+            <a className="w-6 h-6 flex justify-center" href="#">
               <Image src={linkedin} alt="linkding" height={20} />
             </a>
           </div>
