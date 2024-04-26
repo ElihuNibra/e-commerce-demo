d60da4444dfac3266d0e09244db7dfb909881de1
Esteve Paredes <esteve.paredes.4@gmail.com>
2024-04-25 19:26:20 -0500

    Fix responsive style to NewArrival

--- src/app/NewArrival/NewArrival.tsx
+++ src/app/NewArrival/NewArrival.tsx
@@ -6,7 +6,7 @@ import image4 from "../../assets/new-arrival/perfume.svg";
 
 function NewArrival() {
   return (
-    <section className="mb-32 flex justify-center">
+    <section className="mb-32 flex justify-center px-6">
       <div className="grid gap-16">
         <div className="flex flex-col gap-5">
           <div className="flex items-center gap-4">
@@ -15,26 +15,28 @@ function NewArrival() {
           </div>
           <h2 className="font-semibold text-4xl">New Arrival</h2>
         </div>
-        <div className="grid grid-cols-6 grid-rows-4 gap-8">
-          <div className="row-span-4 col-span-3 flex px-8 bg-black">
-            <div className="relative flex">
-              <Image className="self-end" src={image1} alt="play-5" />
-              <div className="text-white absolute bottom-8 left-0 w-64 flex flex-col gap-4">
-                <h3 className="text-2xl font-semibold">PlayStation 5</h3>
-                <p className="text-sm">
-                  Black and White version of the PS5 coming out on sale.
-                </p>
-                <a className="font-medium" href="#">
-                  Shop Now
-                </a>
-              </div>
+        <div className="grid grid:cols-3 grid-rows-8 md:grid-cols-6 md:grid-rows-4 gap-4 md:gap-8">
+          <div className="col-span-3 row-span-4 flex justify-center bg-black relative">
+            <Image className="self-end w-11/12" src={image1} alt="play-5" />
+            <div className="text-white absolute bottom-3 left-3 lg:bottom-6 lg:left-6 w-9/12 flex flex-col gap-2 lg:gap-4">
+              <h3 className="text-lg font-medium lg:text-2xl lg:font-semibold">
+                PlayStation 5
+              </h3>
+              <p className="text-xs lg:text-sm">
+                Black and White version of the PS5 coming out on sale.
+              </p>
+              <a className="font-medium" href="#">
+                Shop Now
+              </a>
             </div>
           </div>
           <div className="col-span-3 row-span-2 flex justify-end bg-black relative">
-            <Image src={image2} alt="women-collection" />
-            <div className="text-white absolute bottom-6 left-6 w-56 flex flex-col gap-4">
-              <h3 className="text-2xl font-semibold">Women’s Collections</h3>
-              <p className="text-sm">
+            <Image className="w-9/12" src={image2} alt="women-collection" />
+            <div className="text-white absolute bottom-3 left-3 lg:bottom-6 lg:left-6 w-6/12 flex flex-col gap-2 lg:gap-4">
+              <h3 className="text-lg font-medium lg:text-2xl lg:font-semibold">
+                Women’s Collections
+              </h3>
+              <p className="text-xs lg:text-sm">
                 Featured woman collections that give you another vibe.
               </p>
               <a className="font-medium" href="#">
@@ -42,22 +44,26 @@ function NewArrival() {
               </a>
             </div>
           </div>
-          <div className="col-span-3 row-span-2 grid grid-cols-2 gap-8">
+          <div className="col-span-3 row-span-2 grid grid-cols-2 gap-4 md:gap-8">
             <div className="flex justify-center bg-black relative">
-              <Image src={image3} alt="" />
-              <div className="text-white absolute bottom-6 left-6 w-48 flex flex-col gap-4">
-                <h3 className="text-2xl font-semibold">Speakers</h3>
-                <p className="text-sm">Amazon wireless speakers</p>
+              <Image className="w-9/12" src={image3} alt="parlante" />
+              <div className="text-white absolute bottom-3 left-3 lg:bottom-6 lg:left-6 w-9/12 flex flex-col gap-2 lg:gap-4">
+                <h3 className="text-lg font-medium lg:text-2xl lg:font-semibold">
+                  Speakers
+                </h3>
+                <p className="text-xs lg:text-sm">Amazon wireless speakers</p>
                 <a className="font-medium" href="#">
                   Shop Now
                 </a>
               </div>
             </div>
             <div className="flex justify-center bg-black relative">
-              <Image src={image4} alt="" />
-              <div className="text-white absolute bottom-6 left-6 w-48 flex flex-col gap-4">
-                <h3 className="text-2xl font-semibold">Perfume</h3>
-                <p className="text-sm">GUCCI INTENSE OUD EDP</p>
+              <Image className="w-9/12" src={image4} alt="perfume" />
+              <div className="text-white absolute bottom-3 left-3 lg:bottom-6 lg:left-6 w-9/12 flex flex-col gap-2 lg:gap-4">
+                <h3 className="text-lg font-medium lg:text-2xl lg:font-semibold">
+                  Perfume
+                </h3>
+                <p className="text-xs lg:text-sm">GUCCI INTENSE OUD EDP</p>
                 <a className="font-medium" href="#">
                   Shop Now
                 </a>
